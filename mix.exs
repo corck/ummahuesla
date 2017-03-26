82a43765fcfe2e0802a244aaa5c63e6975646735
Christoph Klocker <christoph@vedanova.com>
2017-03-26 19:32:41 +0200

    added httpoison

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Ummahuesla.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
+    [applications: [:httpoison]]
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
@@ -28,6 +29,6 @@ defmodule Ummahuesla.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:httpoison, "~> 0.10.0"}]
   end
 end
