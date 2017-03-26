fc59a5041e4eb18474a8ccbf16ff41a5c96fa7f9
Christoph Klocker <christoph@vedanova.com>
2017-03-26 19:32:13 +0200

    Trimming entry

---
+++ lib/ummahuesla/cli.ex
@@ -0,0 +1,27 @@
+defmodule Ummahuesla.CLI do
+
+  def run do
+    %{}
+    |> get_team_name
+    |> get_email
+    |> get_tshirt_size
+    |> IO.inspect
+  end
+
+  defp get_team_name(answers) do
+    ask_question(answers, :team, "Whats your team name")
+  end
+
+  defp get_tshirt_size(answers) do
+    ask_question(answers, :tshirt, "Whats your T-shirt size(s)")
+  end
+
+  defp get_email(answers) do
+    ask_question(answers, :email, "Whats your contact Email")
+  end
+
+  defp ask_question(answers, key, question) do
+    name = IO.gets question <> "\n"
+    Map.put(answers, key, String.trim(name))
+  end
+end
